19171c554f22f1da9c2a9ab738980eee7594d188
alavor <lavor.andrey@gmail.com>
2025-05-28 08:50:19 +0600

    Update

---
+++ intellispaces-core/src/main/java/tech/intellispaces/core/ReflectionImpl.java
@@ -0,0 +1,21 @@
+package tech.intellispaces.core;
+
+class ReflectionImpl implements Reflection {
+  private final Rid rid;
+  private final Domain domain;
+
+  ReflectionImpl(Rid rid, Domain domain) {
+    this.rid = rid;
+    this.domain = domain;
+  }
+
+  @Override
+  public Rid rid() {
+    return null;
+  }
+
+  @Override
+  public Domain domain() {
+    return null;
+  }
+}
--- intellispaces-core/src/main/java/tech/intellispaces/core/Reflections.java
+++ intellispaces-core/src/main/java/tech/intellispaces/core/Reflections.java
@@ -2,6 +2,10 @@ package tech.intellispaces.core;
 
 public interface Reflections {
 
+  static Reflection create(Rid rid, Domain domain) {
+    return new ReflectionImpl(rid, domain);
+  }
+
   static Reflection registered(Reflection reflection, Rid rid) {
     return new RegisteredReflection(reflection, rid);
   }
