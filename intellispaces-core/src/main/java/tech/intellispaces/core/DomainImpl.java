4f68dc6e3bf066a888a71f9ca0597309f4e77bba
alavor <lavor.andrey@gmail.com>
2025-06-02 09:36:06 +0600

    Add DomainImpl#toString

--- intellispaces-core/src/main/java/tech/intellispaces/core/DomainImpl.java
+++ intellispaces-core/src/main/java/tech/intellispaces/core/DomainImpl.java
@@ -41,4 +41,21 @@ class DomainImpl implements Domain {
   public Domain domain() {
     return domain;
   }
+
+  @Override
+  public String toString() {
+    var sb = new StringBuilder();
+    if (name != null) {
+      sb.append(name);
+    } else if (domainClass != null) {
+      sb.append(domainClass.getCanonicalName());
+    }
+    if (rid != null) {
+      if (name != null || domainClass != null) {
+        sb.append(" ");
+      }
+      sb.append("[").append(rid).append("]");
+    }
+    return sb.toString();
+  }
 }
